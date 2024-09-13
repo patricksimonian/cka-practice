0785e1616a7271480982ca253e1a872d5a95511c
unknown <patricksimonian@gmail.com>
2024-09-13 15:53:00 -0700

    complete backup script

--- backup_etcd.sh
+++ backup_etcd.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
 ## First you need to get etcd info
-kubectl describe -n kube-system pod etcd-ckacp01 | grep file 
-
-echo "creating a temp configmap of those files"
+echo "Running ETCD BACKUP"
+echo "creating a temp configmap of etcd cert files. They require reader permissions for the account that is creating the configmap"
 
 kubectl create configmap etcd-certs-temp \
   --from-file=ca.crt=/etc/kubernetes/pki/etcd/ca.crt \
   --from-file=etcd.crt=/etc/kubernetes/pki/etcd/server.crt \
   --from-file=etcd.key=/etc/kubernetes/pki/etcd/server.key
 
+TIMESTAMP=$(date +%F)
+TIMEOUT=$1
+
+if [ -z "$TIMEOUT" ];then
+    printf "Setting Timeout to 30s to wait for pod to be completed by default\n This can be adjusted by running ./backup_etcd.sh 250s \n"
+    TIMEOUT=30s
+fi
 
 ## use etcdctl
 kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Pod
 metadata:
-  name: etcd-backup
+  name: etcd-backup-$TIMESTAMP
 spec:
   restartPolicy: Never   
   securityContext:
@@ -33,7 +39,7 @@ spec:
         - /bin/sh
         - -c
         - |
-          etcdctl snapshot save /backup/etcd_backup_$(date +%F).db \
+          etcdctl snapshot save /backup/etcd_backup_$TIMESTAMP.db \
             --endpoints=https://192.168.1.237:2379 \
             --cacert=/etc/etcd/ca.crt \
             --cert=/etc/etcd/etcd.crt \
@@ -46,9 +52,11 @@ spec:
         name: etcd-certs-temp
 
 EOF
+echo "waiting for pod to be complete"
+kubectl logs "etcd-backup-$TIMESTAMP" -f
+# kubectl wait --for=condition=complete "pod/etcd-backup-$TIMESTAMP" --timeout=$TIMEOUT
 
+echo "======RESULTS====="
+kubectl delete configmap/etcd-certs-temp
 
-# TO DO
-# check if pod is complete
-# output log info
-# delete configmap
+echo "Backup Completed, you may delete the pod by running kubectl delete pod/etcd-backup-$TIMESTAMP"
