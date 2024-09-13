d0982ea310b0c51d80d0f2872a6f6ad61d8dd1f8
unknown <patricksimonian@gmail.com>
2024-09-13 10:43:17 -0700

    update join script

--- join_cluster.sh
+++ join_cluster.sh
@@ -8,16 +8,22 @@ JOIN_COMMAND=$(kubeadm token create --print-join-command)
 TARGET_WORKER=$1
 PASSWORD=$2
 
-sshpass -p "$PASSWORD" ssh "patrick@$TARGET_WORKER" << EOF
+sshpass -p "$PASSWORD" ssh "patrick@$TARGET_WORKER" <<EOF
 # SWAP off must always run when system restarts
 sudo swapoff -a
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF2
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
-$JOIN_COMMAND
+EOF2
+sudo $JOIN_COMMAND
+
 EOF
+
+echo "Complete join"
+
+kubectl get nodes
